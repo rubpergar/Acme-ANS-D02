39485868198ad23d673daf47a9b2e376a07a8853
macpercam15 <macpercam@gmail>
2025-02-25 21:42:27 +0100

    feat: Crear entidad Claim
    re #32

---
+++ src/main/java/acme/entities/claims/Claim.java
@@ -0,0 +1,56 @@
+
+package acme.entities.claims;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Claim extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private claimType			type;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				isAccepted;
+
+}
