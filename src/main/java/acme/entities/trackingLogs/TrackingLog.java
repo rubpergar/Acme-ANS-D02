4a462e7c94510a6b16422b7f1ae0499337d7b564
macpercam15 <macpercam@gmail>
2025-02-25 22:08:58 +0100

    feat: Mandatory student4
    Añadir entidad TrackingLog

---
+++ src/main/java/acme/entities/trackingLogs/TrackingLog.java
@@ -0,0 +1,57 @@
+
+package acme.entities.trackingLogs;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrackingLog extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				lastUpdate;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				stepUndergoing;
+
+	@Mandatory
+	@ValidScore		// Restringe un double de 0 a 100. ?????
+	@Automapped
+	private Double				resolutionPercentage;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				isAccepted;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				reason;
+
+	//Claims need to be tracked through tracking logs. -> debería crear una relación entre claim y trackingLogs??
+}
