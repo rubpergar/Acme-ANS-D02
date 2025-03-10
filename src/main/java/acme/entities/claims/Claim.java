d08b8ea0635565f63cf017d45342375b2f13775d
macpercam15 <macpercam@gmail>
2025-03-10 19:55:02 +0100

    fix: assistance agents como rol, no entidad

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -15,7 +15,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
-import acme.entities.assistenceAgents.AssistanceAgent;
+import acme.realms.AssistanceAgent;
 import lombok.Getter;
 import lombok.Setter;
 
