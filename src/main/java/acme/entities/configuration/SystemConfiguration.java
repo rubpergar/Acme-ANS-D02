69b14d7f8b3c102568fdddc9caacdccfe8cdb67c
macpercam15 <macpercam@gmail>
2025-03-11 18:48:28 +0100

    feat: entity system configuration

--- src/main/java/acme/entities/configuration/SystemConfiguration.java
+++ src/main/java/acme/entities/configuration/SystemConfiguration.java
@@ -1,6 +1,32 @@
+
 package acme.entities.configuration;
 
+import javax.persistence.Entity;
 
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class SystemConfiguration {
 
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{3}$")
+	@Automapped
+	private String				systemCurrency;
+
+	@Mandatory
+	@ValidString(min = 3, max = 255)  //crear un valid currencies ? 
+	@Automapped
+	private String				acceptedCurrencies;
 }
