27f94bd0400fd37e55a430fb9d2c07936d24b2d1
macpercam15 <macpercam@gmail>
2025-03-13 15:53:43 +0100

    feat: form para mi api

---
+++ src/main/java/acme/forms/flightsStatus/FlightDashboard.java
@@ -0,0 +1,31 @@
+
+package acme.forms.flightsStatus;
+
+import java.sql.Date;
+
+import acme.client.components.basis.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FlightDashboard extends AbstractForm {
+
+	//API-> AVIATIONSTACK
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	String						flightNumber;
+	String						airlineName;
+	FlighStatus					flightStatus; //scheduled, active, landed, cancelled, incident,diverted
+	Date						flightDate;
+	String						departureAirport;
+	String						arrivalAirport;
+	Date						arrivalTime;
+	Date						departureTime;
+
+}
